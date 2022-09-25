2435460dcc28df563f00b7e25f6e4e92324939ae
haato3o <me@haato.dev>
2022-09-25 20:25:39 -0300

    Feat: Update Supporter token schema

--- core/persistence/supporter/models.go
+++ core/persistence/supporter/models.go
@@ -1,7 +1,7 @@
 package supporter
 
 type SupporterModel struct {
-	Email    string `json:"email" bson:"email"`
-	Token    string `json:"token" bson:"token"`
-	IsActive bool   `json:"is_active" bson:"is_active"`
+	Email    string `json:"email"`
+	Token    string `json:"token"`
+	IsActive bool   `json:"is_active"`
 }
