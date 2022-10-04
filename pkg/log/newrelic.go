f3fa64525dade2230b3e8ae50f379420c0e51f8e
haato3o <me@haato.dev>
2022-10-04 18:44:08 -0300

    Fix: Trying to fix the coroutine leaks

--- pkg/log/newrelic.go
+++ pkg/log/newrelic.go
@@ -2,8 +2,14 @@ package log
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const ENDPOINT = "https://log-api.newrelic.com/log/v1?Api-Key="
@@ -49,6 +55,17 @@ func (l *NewRelicHeadlessLogger) Error(message string, err error, ctx []*LogCont
 	})
 }
 
+func goid() int {
+	var buf [64]byte
+	n := runtime.Stack(buf[:], false)
+	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
+	id, err := strconv.Atoi(idField)
+	if err != nil {
+		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
+	}
+	return id
+}
+
 func (l *NewRelicHeadlessLogger) send(message NewRelicLogMessage) {
 	if l.apiKey == "" {
 		return
@@ -56,32 +73,40 @@ func (l *NewRelicHeadlessLogger) send(message NewRelicLogMessage) {
 
 	message.App = "poogie-api:prod"
 
+	println("current goroutine:", goid())
 	l.queue <- message
 }
 
 func (l *NewRelicHeadlessLogger) queueListener() {
 	buffer := make([]NewRelicLogMessage, 0)
+	println("current goroutine main listener:", goid())
 
-	for {
-		select {
-		case message := <-l.queue:
-			buffer = append(buffer, message)
+	for message := range l.queue {
+		println("current goroutine listener:", goid())
+		buffer = append(buffer, message)
 
-			if len(buffer) >= BUFFER_SIZE {
-				var payloadBuffer bytes.Buffer
-				encoder := json.NewEncoder(&payloadBuffer)
-				encoder.Encode(buffer)
+		println("num of goroutines:", runtime.NumGoroutine())
 
-				req, err := http.NewRequest(http.MethodPost, ENDPOINT+l.apiKey, &payloadBuffer)
+		if len(buffer) >= BUFFER_SIZE {
+			var payloadBuffer bytes.Buffer
+			encoder := json.NewEncoder(&payloadBuffer)
+			encoder.Encode(buffer)
 
-				if err != nil {
-					Error("failed to flush log buffer", err)
-				}
+			sendThroughHttp(ENDPOINT+l.apiKey, &payloadBuffer)
 
-				http.DefaultClient.Do(req)
-
-				buffer = make([]NewRelicLogMessage, 0)
-			}
+			buffer = make([]NewRelicLogMessage, 0)
 		}
 	}
+}
+
+func sendThroughHttp(endpoint string, buffer *bytes.Buffer) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, buffer)
+
+	if err != nil {
+		Error("failed to flush log buffer", err)
+	}
+
+	http.DefaultClient.Do(req)
 }
