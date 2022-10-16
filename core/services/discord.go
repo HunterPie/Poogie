844c67ced36373720c7b793d4cd4438c503ec38a
haato3o <me@haato.dev>
2022-10-16 15:10:59 -0300

    1.6.3

--- core/services/discord.go
+++ core/services/discord.go
@@ -3,10 +3,23 @@ package services
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"mime/multipart"
 	"net/http"
 	"time"
 )
+
+type DiscordEmbedField struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type DiscordEmbed struct {
+	Title       string              `json:"title"`
+	Description string              `json:"description"`
+	Color       int                 `json:"color"`
+	Fields      []DiscordEmbedField `json:"fields"`
+}
 
 type DiscordWebhookService struct {
 	endpoint string
@@ -14,6 +27,22 @@ type DiscordWebhookService struct {
 
 func NewDiscordWebhookService(endpoint string) *DiscordWebhookService {
 	return &DiscordWebhookService{endpoint: endpoint}
+}
+
+func (s *DiscordWebhookService) SendEmbed(embed DiscordEmbed) {
+	var buffer bytes.Buffer
+	encoder := json.NewEncoder(&buffer)
+	encoder.Encode(embed)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	req, _ := http.NewRequestWithContext(ctx, http.MethodPost, s.endpoint, &buffer)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	client.Do(req)
 }
 
 func (s *DiscordWebhookService) Send(data string) {
--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,3 +1,3 @@
 package metadata
 
-const Version string = "1.6.2"
+const Version string = "1.6.3"
